501e1326ee9502abe9d6a0ccd39752b63425a286
daiyan1998 <abduddaiyan7@gmail.com>
2023-08-05 00:21:48 +0600

    finished server site setup

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -6,4 +6,19 @@ const notFound = (req, res, next) => {
 
 const errorHandler = (err, req, res, next) => {
   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
+
+  //   Check for Mongoose bad ObjectID
+  if (err.name === "CastError" && err.kind === "ObjectId") {
+    console.log(err.name);
+    message = `Resource not found`;
+    statusCode = 404;
+  }
+
+  res.status(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === "production" ? "🥮" : err.stack,
+  });
 };
+
+export { notFound, errorHandler };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,26 +1,10 @@
 import express from "express";
 const router = express.Router();
-import asysnHandler from "../middleware/asyncHandler.js";
-import Product from "../models/productModel.js";
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productController.js";
 
-router.get(
-  "/",
-  asysnHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-
-router.get(
-  "/:id",
-  asysnHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    }
-
-    res.status(404).json({ message: "Product not found" });
-  })
-);
-
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 export default router;
